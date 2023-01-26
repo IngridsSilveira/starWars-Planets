bc90b8c17630babfcd9f5e2fe2060f44838a8d49
IngridsSilveira <ingridsouzaok@gmail.com>
2023-01-26 13:21:10 -0300

    imagens add

--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -1,21 +1,25 @@
-const teste = {
-  images: "https://raw.githubusercontent.com/IngridsSilveira/starWars-Planets/main/assets/images/planetas/tatooine.jpeg",
-  
-}
-console.log(teste)
+const teste = [
+  "https://raw.githubusercontent.com/IngridsSilveira/starWars-Planets/main/assets/images/planetas/tatooine.jpeg",
+  "https://raw.githubusercontent.com/IngridsSilveira/starWars-Planets/main/assets/images/planetas/alderann.webp",
+];
+const imagens = teste;
+imagens.map((elemento) => {
+  console.log(elemento);
+});
 const divPreencher = document.getElementById("preencher");
 
-const criandoElementos = (name, climate) => {
+const criandoElementos = (name, climate, image) => {
   const novoElemento = document.createElement("div");
   const p_planeta = document.createElement("p");
   const p_clima = document.createElement("p");
-  const img = document.createElement("img")
+  const img = document.createElement("img");
 
-  novoElemento.setAttribute("class", "novoElemento")
-  p_planeta.setAttribute("class", "p_planeta")
-  p_clima.setAttribute("class", "p_clima")
-  img.setAttribute("class", "imagens")
-  img.src = `${teste.images}`
+  novoElemento.setAttribute("class", "novoElemento");
+  p_planeta.setAttribute("class", "p_planeta");
+  p_clima.setAttribute("class", "p_clima");
+  img.setAttribute("class", "imagens");
+
+  img.src = `${image}`
 
   p_planeta.innerHTML = name;
   p_clima.innerHTML = "Clima: " + climate;
@@ -23,7 +27,7 @@ const criandoElementos = (name, climate) => {
   divPreencher.appendChild(novoElemento);
   novoElemento.appendChild(p_planeta);
   novoElemento.appendChild(p_clima);
-  novoElemento.appendChild(img)
+  novoElemento.appendChild(img);
 };
 
 const funcaoAssincrona = async (url) => {
@@ -32,7 +36,7 @@ const funcaoAssincrona = async (url) => {
   const capturandoItens = jsonBody.results;
   capturandoItens.map((elemento) => {
     const { name, climate } = elemento;
-    criandoElementos(name, climate);
+    criandoElementos(name, climate, teste);
   });
 };
 funcaoAssincrona("https://swapi.dev/api/planets/");
