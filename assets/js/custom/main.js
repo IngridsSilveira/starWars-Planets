6129788439f038177fc3d099085c67fa7ef64c9a
IngridsSilveira <ingridsouzaok@gmail.com>
2023-01-28 11:07:53 -0300

    novos itens add, como terreno, populacao, etc

--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -1,21 +1,28 @@
-const URL = "image.json"
 const divPreencher = document.getElementById("preencher");
 
-const criandoElementos = (name, climate) => {
+const criandoElementos = (name, climate, terrain, population) => {
   const novoElemento = document.createElement("div");
   const p_planeta = document.createElement("p");
   const p_clima = document.createElement("p");
+  const p_terreno = document.createElement("p");
+  const p_population = document.createElement("p");
 
   novoElemento.setAttribute("class", `${name}`)
-  p_planeta.setAttribute("class", "p_planeta");
-  p_clima.setAttribute("class", "p_clima");
+  p_planeta.setAttribute("class", "title_planeta");
+  p_clima.setAttribute("class", "p_padrao");
+  p_terreno.setAttribute("class", "p_padrao");
+  p_population.setAttribute("class", "p_padrao");
 
   p_planeta.innerHTML = name;
   p_clima.innerHTML = "Clima: " + climate;
+  p_terreno.innerHTML = "Terreno: " + terrain;
+  p_population.innerHTML = "População: " + population;
 
   divPreencher.appendChild(novoElemento)
   novoElemento.appendChild(p_planeta);
   novoElemento.appendChild(p_clima);
+  novoElemento.appendChild(p_terreno);
+  novoElemento.appendChild(p_population);
 };
 
 const funcaoAssincrona = async (url) => {
@@ -23,9 +30,8 @@ const funcaoAssincrona = async (url) => {
   const jsonBody = await resposta.json();
   const capturandoItens = jsonBody.results;
   capturandoItens.map((elemento) => {
-    console.log(elemento)
-    const { name, climate } = elemento;
-    criandoElementos(name, climate)
+    const { name, climate, terrain, population } = elemento;
+    criandoElementos(name, climate, terrain, population)
   })
 };
 
